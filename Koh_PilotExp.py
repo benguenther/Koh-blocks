beab7e23069a31504171c85b51ce8448b90a1636
benguenther <74007340+benguenther@users.noreply.github.com>
2025-03-10 12:19:58 -0400

    Update Koh_PilotExp.py

--- Koh_PilotExp.py
+++ Koh_PilotExp.py
@@ -5,18 +5,43 @@
 
 from psychopy import visual, event
 from KohBlocks import KohBlock
-import math, random
+import math, random, sys
 
-win = visual.Window(
-    monitor="hp_home_main", #"surface", #"testMonitor",  # "BlackLaptop",,
-    fullscr=True,
-    size=[1920, 1080],  # [1920, 1080],# [1280, 1024], #[2736, 1824], #[1600, 900],
-    screen=0,
-    color=[+1] * 3,
-    units="pix"
-)
 
-condition = "test"
+condition = "far"
+
+# define the stimulus presentation window
+if condition == "test":
+    win = visual.Window(
+        monitor="hp_home_main", #"surface", #"testMonitor",  # "BlackLaptop",,
+        fullscr=True,
+        size=[1920, 1080],  # [1920, 1080],# [1280, 1024], #[2736, 1824], #[1600, 900],
+        screen=0,
+        color=[+1] * 3,
+        units="pix"
+    )
+elif condition == "near":
+    win = visual.Window(
+        monitor="LG",
+        fullscr=True,
+        size=[3840, 2160],
+        screen=1,
+        color=[+1] * 3,
+        units="pix"
+    )
+elif condition == "far":
+    win = visual.Window(
+        monitor="BigTV",
+        fullscr=True,
+        size=[3840, 2160],
+        screen=1,
+        color=[+1] * 3,
+        units="pix"
+    )
+else:
+    sys.exit("error in condition settings")
+
+
 
 # function to convert stimulus dimensions from visual angle to pixels
 def visual_angle(deg, cond = condition):
@@ -30,8 +55,8 @@ def visual_angle(deg, cond = condition):
         distance = 5385
     elif cond == "near":
         screen_pix = [3840, 2160]
-        screen_mm = [529, 297] # need to measure and change this!!!
-        distance = 400 # beed to check/confirm
+        screen_mm = [597, 335] 
+        distance = 400  
     # assumes pixels are squaare
     pix_mm = (screen_pix[0]/screen_mm[0])
     pix = (2 * distance) * math.tan((deg * (math.pi/180))/2) * pix_mm
