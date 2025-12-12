3eb29d1e0c9dd50c8ec35ae1d5711eeef32458a6
benguenther <74007340+benguenther@users.noreply.github.com>
2025-12-12 10:00:25 -0500

    run file and id list
    Added a run file and list of preset participant IDs

---
+++ run_experiment.py
@@ -0,0 +1,3 @@
+# select the play icon (upper right of screen) to run the experiment
+# import Koh_PilotExp  (this is just a backup bit of text to show what the code to run the experiment should look like)
+import Koh_PilotExp
